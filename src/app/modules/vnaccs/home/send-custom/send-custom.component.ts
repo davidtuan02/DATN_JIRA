fbb05c6b62cf4f5e720363d5a9d51c2d0f319e25
nguyenvantuan <nguyenvantuan@techasians.com>
2024-10-25 17:29:59 +0700

    update: search custom

--- src/app/modules/vnaccs/home/send-custom/send-custom.component.ts
+++ src/app/modules/vnaccs/home/send-custom/send-custom.component.ts
@@ -1,47 +1,258 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+import { NzFormModule } from 'ng-zorro-antd/form'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { NzModalModule, NzModalRef } from 'ng-zorro-antd/modal'
+import { NzSelectModule } from 'ng-zorro-antd/select'
 import { NzStepsModule } from 'ng-zorro-antd/steps'
+import { NotificationService } from '../../../../shared/services/notification.service'
+import { LoginService } from '../../login/login.service'
+import { NzTableModule } from 'ng-zorro-antd/table'
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip'
+import { NzRadioModule } from 'ng-zorro-antd/radio'
+import { RouterLink } from '@angular/router'
+import { DialogService } from '../../../../shared/services/dialog.service'
+import { ConfirmPopupComponent } from '../../../../shared/components/confirm-popup/confirm-popup.component'
 
 @Component({
   selector: 'app-send-custom',
   templateUrl: './send-custom.component.html',
   styleUrls: ['./send-custom.component.scss'],
   standalone: true,
-  imports: [NzStepsModule, CommonModule]
+  imports: [
+    NzStepsModule,
+    CommonModule,
+    NzFormModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzSelectModule,
+    NzModalModule,
+    NzTableModule,
+    NzToolTipModule,
+    NzRadioModule,
+    RouterLink
+  ]
 })
 export class SendCustomComponent {
   current = 0
 
   index = 'First-content'
 
-  pre(): void {
-    this.current -= 1
-    this.changeContent()
+  optionRepresent = [
+    {
+      value: 1,
+      label: 'CMND'
+    },
+    {
+      value: 2,
+      label: 'CCCD'
+    },
+    {
+      value: 3,
+      label: 'Hộ chiếu'
+    }
+  ]
+
+  radioValue = '1'
+  isVisible: boolean = false
+  isVisibleDownload: boolean = false
+  isOkLoading = false
+
+  msAcc = ''
+  listOfData: any[] = []
+
+  optionFileStatuss = [
+    { value: 1, label: 'Đang hiển thị' },
+    { value: 0, label: 'Đang tắt' }
+  ]
+
+  form!: FormGroup
+  modalTitleDownload: string = 'Tải ứng dụng di động để đăng ký MySign'
+  modalTitle: string = 'Lấy chứng thư số'
+  isAbleBtnSign: boolean = false
+
+  constructor(
+    private notification: NotificationService,
+    private loginSrv: LoginService,
+    private fb: FormBuilder,
+    private dialogSrv: DialogService
+  ) {}
+
+  ngOnInit() {
+    this.loadForm()
   }
 
-  next(): void {
-    this.current += 1
-    this.changeContent()
+  loadForm() {
+    this.form = this.fb.group({
+      fullName: [''],
+      registerPlace: [null],
+      digitalSignatureType: [this.radioValue],
+      digitalSignature: [''],
+      serial: [''],
+      provider: [''],
+      effectiveDate: [''],
+      expiryDate: [''],
+      publicKey: [''],
+      nameCert: [''],
+      credentialId: ['']
+    })
+    this.form.get('digitalSignature')?.disable()
+    this.form.get('serial')?.disable()
+    this.form.get('provider')?.disable()
+    this.form.get('effectiveDate')?.disable()
+    this.form.get('expiryDate')?.disable()
+    this.form.get('publicKey')?.disable()
+    this.form.get('nameCert')?.disable()
+  }
+
+  pre(): void {
+    this.current -= 1
   }
 
   done(): void {
     console.log('done')
   }
 
-  changeContent(): void {
-    switch (this.current) {
-      case 0: {
-        this.index = 'First-content'
-        break
+  sendCustom() {
+    const data = {
+      title: 'Bạn có muốn gửi tới Hải quan không?'
+    }
+    const dialogRef = this.dialogSrv.openDialog(ConfirmPopupComponent, '', data, {
+      nzClosable: false,
+      nzWidth: '400px',
+      nzCentered: true,
+      nzClassName: 'popup-radius-2'
+    })
+    dialogRef.afterClose.subscribe((result: any) => {
+      if (result) {
+        this.current += 1
       }
-      case 1: {
-        this.index = 'Second-content'
-        break
-      }
-      default: {
-        this.index = 'error'
-      }
+    })
+  }
+
+  copyText(): void {
+    const inputElement = document.getElementById('copyInput') as HTMLInputElement
+    if (inputElement) {
+      inputElement.select()
+      inputElement.setSelectionRange(0, 99999)
+      document.execCommand('copy')
+      this.notification.success('Đã sao chép đường dẫn')
     }
   }
-  constructor() {}
+  navigateToDownload(store: string): void {
+    if (store === 'appstore') {
+      window.open('https://apps.apple.com/vn/app/mysign/id1633019232', '_blank')
+    }
+    if (store === 'chplay') {
+      window.open('https://play.google.com/store/apps/details?id=com.viettel.cloud.ca.mysign&hl=vi', '_blank')
+    }
+  }
+
+  showModal() {
+    this.isVisible = true
+    this.msAcc = ''
+    this.listOfData = []
+  }
+  showModalDownload() {
+    this.isVisible = false
+    this.isVisibleDownload = true
+  }
+
+  handleOk() {
+    this.isVisible = false
+  }
+
+  handleCancel(): void {
+    this.isVisible = false
+  }
+
+  handleOkDownload() {
+    this.isVisibleDownload = false
+    // this.router.navigate(['vnaccs/register']);
+  }
+
+  handleCancelDownload(): void {
+    this.isVisibleDownload = false
+    // this.router.navigate(['vnaccs/register']);
+  }
+
+  getCTS() {
+    if (this.msAcc === '') {
+      this.notification.error('Vui lòng nhập tài khoản MySign để lấy chứng thư số')
+    } else {
+      this.loginSrv.getCertInfo(this.msAcc).subscribe((res: any) => {
+        if (res) {
+          if (res.message === 'success') {
+            this.listOfData = res.data
+          } else {
+            this.notification.error(
+              'Có lỗi xảy ra khi kết nối với hệ thống Viettel - MySign. Vui lòng thử lại hoặc liên hệ quản trị viên'
+            )
+          }
+        } else {
+          this.notification.error(
+            'Có lỗi xảy ra khi kết nối với hệ thống Viettel - MySign. Vui lòng thử lại hoặc liên hệ quản trị viên'
+          )
+        }
+      })
+    }
+  }
+
+  convertComplexDateString(dateStr: string): number {
+    // Chuyển chuỗi 'YYYYMMDDHHmmss±zzzz' thành định dạng ISO 8601
+    const isoFormattedDate = `${dateStr.slice(0, 4)}-${dateStr.slice(4, 6)}-${dateStr.slice(6, 8)}T${dateStr.slice(
+      8,
+      10
+    )}:${dateStr.slice(10, 12)}:${dateStr.slice(12, 14)}${dateStr.slice(14)}`
+
+    // Tạo đối tượng Date từ chuỗi đã định dạng
+    const dateObj = new Date(isoFormattedDate)
+
+    // Trả về timestamp (số milliseconds từ epoch)
+    return dateObj.getTime()
+  }
+
+  formatDateFromString = (dateString: string): string | null => {
+    if (dateString.length < 8) {
+      return null
+    }
+    const year = dateString.substring(0, 4)
+    const month = dateString.substring(4, 6)
+    const day = dateString.substring(6, 8)
+
+    const date = new Date(`${year}-${month}-${day}`)
+
+    if (isNaN(date.getTime())) {
+      return null
+    }
+
+    return `${day}/${month}/${year}`
+  }
+
+  applyData(data: any) {
+    const currentDate = new Date().getTime()
+    const validDate = this.convertComplexDateString(data.validFrom)
+    const expireDate = this.convertComplexDateString(data.validTo)
+    if (validDate > currentDate) {
+      console.log('Chữ ký số chưa có hiệu lực')
+      this.notification.error('Chữ ký số chưa có hiệu lực')
+    } else if (expireDate < currentDate) {
+      console.log('Chữ ký số đã hết hiệu lực')
+      this.notification.error('Chữ ký số đã hết hiệu lực')
+    } else {
+      this.isVisible = false
+      // this.loginForm.get('digitalSignature')?.setValue(data.subjectDN);
+      this.form.get('serial')?.setValue(data.serialNumber)
+      this.form.get('provider')?.setValue(data.issuerDN)
+      this.form.get('effectiveDate')?.setValue(this.formatDateFromString(data.validFrom))
+      this.form.get('expiryDate')?.setValue(this.formatDateFromString(data.validTo))
+      this.form.get('nameCert')?.setValue(data.subjectDN)
+      this.form.get('publicKey')?.setValue(data.subjectDN) //check
+      this.form.get('credentialId')?.setValue(data.credentialId) //check
+      // this.loginForm.enable()
+      // this.disableForm();
+    }
+  }
 }
